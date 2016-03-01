322dff66869708d20c8f341bedb46b1f1b252b0e
Maximilian Nickel <mnick@mit.edu>
2016-03-01 12:56:36 -0500

    fix unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,19 +12,19 @@ main = hspec $ do
 
   describe "Sparse.Vector" $ do
     it "dot product" $ do
-      let x = V.fromAssocList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
-      let y = V.fromAssocList [(1, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
-      let z = V.fromAssocList [(2, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
+      let x = V.fromList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
+      let y = V.fromList [(1, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
+      let z = V.fromList [(2, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
       x <> y       `shouldBe` 6.0
       x <> z       `shouldBe` 0
       y <> z       `shouldBe` 9.0
       x <> V.empty `shouldBe` 0
 
     it "equality" $ do
-      let x  = V.fromAssocList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
-      let x' = V.fromAssocList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
-      let y  = V.fromAssocList [(1, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
-      let z  = V.fromAssocList [(4, 3.0)] :: (SparseVector 10 Double)
+      let x  = V.fromList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
+      let x' = V.fromList [(1, 2.0), (4, 3.0)] :: (SparseVector 10 Double)
+      let y  = V.fromList [(1, 3.0), (6, 3.0)] :: (SparseVector 10 Double)
+      let z  = V.fromList [(4, 3.0)] :: (SparseVector 10 Double)
 
       x == x'      `shouldBe` True
       x == y       `shouldBe` False
@@ -33,11 +33,11 @@ main = hspec $ do
 
   describe "Sparse.Matrix" $ do
     it "equality" $ do
-      let m  = M.fromAssocList [((2,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
-      let m' = M.fromAssocList [((2,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
-      let x  = M.fromAssocList [((1,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
-      let y  = M.fromAssocList [((2,1),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
-      let z  = M.fromAssocList [((2,4),0.2), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
+      let m  = M.fromList [((2,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
+      let m' = M.fromList [((2,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
+      let x  = M.fromList [((1,4),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
+      let y  = M.fromList [((2,1),0.5), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
+      let z  = M.fromList [((2,4),0.2), ((7,3), 0.1)] :: (SparseMatrix 10 7 Double)
 
       m == m' `shouldBe` True
       m == x  `shouldBe` False
